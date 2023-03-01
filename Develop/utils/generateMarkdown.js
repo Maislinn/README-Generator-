48838396776a1cd684f187f365a0c0b99463fb9f
Maislinn <maislinn.helfer@gmail.com>
2023-02-28 17:47:54 -0800

    Updated the generateMarkdown.js file to include the license badge and link. Also updated the package-lock.json file to include the license badge and link.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,61 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === "MIT") {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  let licenseBadge = "";
+  switch (license) {
+    case "MIT":
+      licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "Apache 2.0":
+      licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GNU GPLv3":
+      licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "BSD 3":
+      licenseBadge = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "None":
+      licenseBadge = "";
+      break;
   }
-   else if (license === "Apache 2.0") {
-    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-  } 
-  else if (license === "GNU GPLv3") {
-    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-  } 
-  else if (license === "BSD 3") {
-    return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
-  } 
-  else {
-    return "";
-}
+  return licenseBadge;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license == "MIT") {
-    return `https://opensource.org/licenses/MIT`;
-  } 
-  if (license == "Apache 2.0") {
-    return `https://opensource.org/licenses/Apache-2.0`;
+  let licenseLink = "";
+  switch (license) {
+    case "MIT":
+      licenseLink = "[MIT](https://opensource.org/licenses/MIT)";
+      break;
+    case "Apache 2.0":
+      licenseLink = "[Apache 2.0](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GNU GPLv3":
+      licenseLink = "[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "BSD 3":
+      licenseLink = "[BSD 3](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "None":
+      licenseLink = "";
+      break;
   }
-  if (license == "GNU GPLv3") {
-    return `https://www.gnu.org/licenses/gpl-3.0`;
-  }
-  if (license == "BSD 3") {
-    return `https://opensource.org/licenses/BSD-3-Clause`;
-  }
-  if (license == "None") {
-    return "";
-  }
+  return licenseLink;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license == "MIT") {
-    return `This project is licensed under the ${license} license.`;
-  } 
-  if (license == "Apache 2.0") {
-    return `This project is licensed under the ${license} license.`;
-  }
-  if (license == "GNU GPLv3") {
-    return `This project is licensed under the ${license} license.`;
-  }
-  if (license == "BSD 3") {
-    return `This project is licensed under the ${license} license.`;
-  }
-  if (license == "None") {
-    return "";
-  }
+  let licenseSection = "";
+    if (license == "MIT") {
+      return "This project is licensed under the ${license} license.";
+    } 
+    if (license == "Apache 2.0") {
+      return "This project is licensed under the ${license} license.";
+    }
+    if (license == "GNU GPLv3") {
+      return "This project is licensed under the ${license} license.";
+    }
+    if (license == "BSD 3") {
+      return "This project is licensed under the ${license} license.";
+    }
+    if (license == "None") {
+      return "";
+    }
 }
 
 // TODO: Create a function to generate markdown for README
